661cd9d784aa941abe411d6a82d3896988c47ca1
José Enrique <otipip90@gmail.com>
2021-03-07 15:21:32 -0500

    added modal to our app.

--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
-import { View, Text, Button, FlatList, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, Modal, StyleSheet } from 'react-native';
 import { globalStyles } from "../styles/global";
 import Card from '../shared/card';
+import { MaterialIcons } from "@expo/vector-icons";
+
 
 export default function Home({ navigation }) {
+
+    const [modalOpen, setModalOpen] = useState(false);    
     const [reviews, setReviews] = useState([
         { title:'Zelda, Breath of Fresh Air', rating: 5, body:'lorem ipsum', key:'1' },
         { title:'Gotta Catch Them All (again)', rating: 4, body:'lorem ipsum', key:'2' },
@@ -13,6 +17,24 @@ export default function Home({ navigation }) {
 //navigate for the component name
     return (
         <View style={globalStyles.container}>
+
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <MaterialIcons 
+                        name='close'
+                        size={24}
+                        style={{...styles.modalToggle,...styles.modalClose }}
+                        onPress={()=>setModalOpen(false)}
+                    />
+                    <Text>Hello from Modal :)</Text>
+                </View>
+            </Modal>
+            <MaterialIcons 
+                        name='add'
+                        size={24}
+                        style={styles.modalToggle}
+                        onPress={()=>setModalOpen(true)}
+                    />
             <FlatList
                 data={reviews}
                 renderItem={( {item} ) =>(
@@ -27,3 +49,21 @@ export default function Home({ navigation }) {
     )
 }
 
+
+const styles = StyleSheet.create({
+    modalContent:{
+        flex:1
+    },
+    modalToggle:{
+        marginBottom: 10,
+        borderWidth:1,
+        borderColor:'#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf:'center'
+    },
+    modalClose:{
+        marginTop:20,
+        marginBottom:0
+    }
+});
