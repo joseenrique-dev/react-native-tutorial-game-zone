3b4111f2e3944e937906ae62679a059a7c1f7852
José Enrique <otipip90@gmail.com>
2021-03-07 12:01:26 -0500

    created shared styles card.

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, Button} from 'react-native';
 import { globalStyles } from "../styles/global";
+import Card from '../shared/card';
 
 export default function ReviewDetails({ route, navigation }) {
 
@@ -11,8 +12,10 @@ export default function ReviewDetails({ route, navigation }) {
     const { title, raiting } = route.params;
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>{ title }</Text>
-            <Text style={globalStyles.titleText}>{ raiting }</Text>
+            <Card>
+                <Text style={globalStyles.titleText}>{ title }</Text>
+                <Text style={globalStyles.titleText}>{ raiting }</Text>
+            </Card>
         </View>
     )
 }
