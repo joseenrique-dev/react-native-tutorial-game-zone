283b52073a74fd450286e673b007ee791758e470
José Enrique <otipip90@gmail.com>
2021-03-06 20:16:45 -0500

    custom fonts, install @expo-google-fonts/inter and @expo-google-fonts/inter@0.1.0

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from "./screens/home";
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  });
 
 export default function App() {
-  return (
-    <Home />
-  );
+  const [ fontLoaded, setFontLoaded ] = useState(false);
+  
+  let [fontsLoaded] = useFonts({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <Home />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
